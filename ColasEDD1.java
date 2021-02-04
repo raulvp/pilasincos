2aed019131c0a8686a386e363167fa1132a2dac8
Raul Vera Portanda <raulvp9@gmail.com>
2021-02-04 08:20:51 +0400

    Create ColasEDD1.java

---
+++ ColasEDD1.java
@@ -0,0 +1,52 @@
+
+
+package colasedd1;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ColasEDD1 {
+
+   
+    public static void main(String[] args) {
+        Queue<Integer> colita = new LinkedList<Integer>(); //creando la cola
+        
+        System.out.println("El cliente a ser atendido: "
+                +cliente.peek().getNombre()+" con ticket "
+                +cliente.peek().getTicket());
+        
+        colita.add(1);
+        colita.add(2);
+        colita.add(3);
+        colita.add(4);
+        colita.add(5);
+        colita.add(6);
+        colita.add(7);
+        colita.add(8);
+        colita.add(9);
+        colita.add(10);
+        
+        System.out.println("El ticket a ser atendido: "
+                +colita.peek());
+        
+        System.out.println("El tamaño de la cola: "
+                +colita.size());
+        
+        System.out.println("Esta vacia la cola? "
+                +colita.isEmpty());
+        
+        //desencolar
+        colita.remove();
+        System.out.println("El ticket a ser atendido: "
+                +colita.peek());
+        
+        //recorrido
+        
+        while(!colita.isEmpty()){
+            System.out.println("Atendiendo al ticket: "
+                    +colita.remove());
+        }
+                 
+    }
+    
+}
